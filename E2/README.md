b4eab0139d22593e5a12ea6bf77ccb88ec6a899c
Carter Jackson <carterjackson96@gmail.com>
2018-02-27 12:44:49 -0500

    E2
    E2 add

---
+++ E2/README.md
@@ -0,0 +1,2 @@
+A simple shell that takes the path to an executable as input, then runs it.
+* Looks for lines ending in '\\t\\n'
---
+++ E2/simple_shell.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+void main(){
+	int running = 1;
+	while(running){
+		char input[100];
+		char **argv = malloc(50 * sizeof(char*));
+		char *arg;
+		char path[100];
+
+		putchar('%');
+		fgets(input,100,stdin);
+		arg = strtok(input," \t\n");
+		strcpy(path,"./");
+		strcat(path,arg);
+		int i = 0;
+		while(arg != NULL){
+			arg = strtok(NULL," \t\n");
+			argv[i] = arg;
+			i++;
+		}
+
+		pid_t childPID = fork();
+		if(childPID == 0){
+			execvp(path,argv);
+		}else{
+			wait(NULL);
+		}
+		putchar('\n');
+	}
+}
